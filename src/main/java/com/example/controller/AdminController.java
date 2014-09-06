bdf85e503f70e6d161812f343e1e4aa5818318bd
sudeep cv <deepam23590@gmail.com>
2014-09-06 12:00:40 +0530

    controller edited

--- src/main/java/com/example/controller/AdminController.java
+++ src/main/java/com/example/controller/AdminController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AdminController {
 
-    @RequestMapping("/addpost.jsp")
+    @RequestMapping("/")
     public String addPost() {
 
 
