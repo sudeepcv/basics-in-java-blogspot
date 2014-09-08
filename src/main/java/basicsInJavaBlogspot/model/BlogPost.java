05068210c0e8d1cbcc0710fa2f79c036c28056ff
sudeep cv <deepam23590@gmail.com>
2014-09-08 09:56:56 +0530

    model

---
+++ src/main/java/basicsInJavaBlogspot/model/BlogPost.java
@@ -0,0 +1,45 @@
+package basicsInJavaBlogspot.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class BlogPost {
+	@Id	
+	@GeneratedValue
+	private Long Id;
+	private String Title;
+	@Column(columnDefinition="TEXT")
+	private String Content;
+	private Date Date;
+	public String getContent() {
+		return Content;
+	}
+	public Date getDate() {
+		return Date;
+	}
+	public Long getId() {
+		return Id;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setContent(String content) {
+		Content = content;
+	}
+	public void setDate(Date date) {
+		Date = date;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	
+
+}
