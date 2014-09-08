4d01540f63075ea7686e4cb5228380f2986747b9
sudeep cv <deepam23590@gmail.com>
2014-09-08 11:16:15 +0530

    edited controller

--- src/main/java/basicsInJavaBlogspot/controller/AdminController.java
+++ src/main/java/basicsInJavaBlogspot/controller/AdminController.java
@@ -2,18 +2,15 @@ package basicsInJavaBlogspot.controller;
 
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import basicsInJavaBlogspot.model.BlogPost;
-import basicsInJavaBlogspot.repository.BlogRepository;
 @Controller
 public class AdminController {
-	@Autowired
-	private BlogRepository blogRepository;
+	
 	
     @RequestMapping("/addpost")
     public String addpost() {
@@ -27,7 +24,7 @@ public class AdminController {
     	blogpost.setContent(content);
     	blogpost.setTitle(title);
     	blogpost.setDate(new Date());
-    	blogRepository.save(blogpost);
+    	
     	return "addpost";
     }
 
