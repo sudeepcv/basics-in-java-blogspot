229cffd90c2dc6fc413967db1c527134b0e432af
sudeep cv <deepam23590@gmail.com>
2014-09-13 14:32:56 +0530

    transactional

--- src/main/java/basicsInJavaBlogspot/controller/AdminController.java
+++ src/main/java/basicsInJavaBlogspot/controller/AdminController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,7 @@ public class AdminController {
 		return "index";
 	}
 	@RequestMapping(value="/{postTitle}")
+	@Transactional
 	public String blogPost(@PathVariable("postTitle") String postTitle,Model model){
 		List<BlogPost> singlePost=blogRepository.findByTitleEquals(postTitle);
 		//model.addAttribute("singlePost",singlePost);
