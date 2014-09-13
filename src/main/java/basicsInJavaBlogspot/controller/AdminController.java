eac99ac4ff3a90bace81df012d505daa76c47d85
sudeep cv <deepam23590@gmail.com>
2014-09-13 09:40:01 +0530

    response body as string json

--- src/main/java/basicsInJavaBlogspot/controller/AdminController.java
+++ src/main/java/basicsInJavaBlogspot/controller/AdminController.java
@@ -59,7 +59,7 @@ public class AdminController {
     public @ResponseBody String deletPost(@RequestParam("postId") Long postId){
     	BlogPost deletedPost=blogRepository.findOne(postId);
     	blogRepository.delete(postId);
-    	return "{successid:" +postId+"}";
+    	return "{\"successId\":"+postId+"}";
     }
 
 }
