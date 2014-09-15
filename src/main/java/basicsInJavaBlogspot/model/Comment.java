bca0a24fa87cfdda254e2700ef49d8f8ff47edb0
sudeep cv <deepam23590@gmail.com>
2014-09-15 09:22:17 +0530

    forgot to add id in model

--- src/main/java/basicsInJavaBlogspot/model/Comment.java
+++ src/main/java/basicsInJavaBlogspot/model/Comment.java
@@ -1,12 +1,15 @@
 package basicsInJavaBlogspot.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
 public class Comment {
-	
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String name;
 	private String content;
