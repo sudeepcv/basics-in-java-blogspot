7143b477c15be85171e8d6ffc91dc5cb7a6ded56
sudeep cv <deepam23590@gmail.com>
2014-09-16 12:09:07 +0530

    param

--- src/main/java/basicsInJavaBlogspot/repository/BlogRepository.java
+++ src/main/java/basicsInJavaBlogspot/repository/BlogRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import basicsInJavaBlogspot.model.BlogPost;
@@ -12,8 +13,8 @@ import basicsInJavaBlogspot.model.BlogReport;
 public interface BlogRepository extends JpaRepository<BlogPost,Long> {
 
 	BlogPost findByTitleEquals(String postTitle);
-    @Query("Select new basicsInJavaBlogspot.model.BlogReport(b.title,b.content,b.date,c.name,c.content) from BlogPost b,Comment c where b.id=c.blogPost.id AND b.id=:postTitle")
-	List<BlogReport> getBlogReport(String postTitle);
+    @Query("Select new basicsInJavaBlogspot.model.BlogReport(b.title,b.content,b.date,c.name,c.content) from BlogPost b,Comment c where b.id=c.blogPost.id AND b.id=:blogId")
+	List<BlogReport> getBlogReport(@Param("blogId") Long blogId);
 
 
 }
