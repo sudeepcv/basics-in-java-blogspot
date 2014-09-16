866a376816cee33e3f5d05b1e91de349735bbaca
sudeep cv <deepam23590@gmail.com>
2014-09-16 15:39:21 +0530

    sfasdf

--- src/main/java/basicsInJavaBlogspot/repository/BlogRepository.java
+++ src/main/java/basicsInJavaBlogspot/repository/BlogRepository.java
@@ -13,7 +13,7 @@ import basicsInJavaBlogspot.model.BlogReport;
 public interface BlogRepository extends JpaRepository<BlogPost,Long> {
 
 	BlogPost findByTitleEquals(String postTitle);
-    @Query("Select new basicsInJavaBlogspot.model.BlogReport(b.title,b.content,b.date,c.name,c.content) from BlogPost b,Comment c where b.id=c.blogPost.id AND b.id=:blogId")
+    @Query("Select new basicsInJavaBlogspot.model.BlogReport(b.title,b.content,b.date,c.name,c.content) from BlogPost b,Comment c where b.id=c.blogPost.id AND b.id=:blogId c.status=true")
 	List<BlogReport> getBlogReport(@Param("blogId") Long blogId);
 
 
